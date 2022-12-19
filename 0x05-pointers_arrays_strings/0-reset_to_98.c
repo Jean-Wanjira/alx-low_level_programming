2cee9d0f3e4be7d24cdd771f852113776d4542dc
Wanjira <jeanwanjira2802@gmail.com>
2022-12-19 14:47:58 -0800

    takes a pointer to an int as parameter and updates the value it points to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 /**
  *   reset_to_98- takes a pointer to an int as parameter
  *   and updates the value it points to 98
+ *   @n- the pointer to an int
 */
-
 void reset_to_98(int *n)
 {
 *n = 98;	
