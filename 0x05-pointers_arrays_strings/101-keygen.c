e49a33a503eb912a4b2e1ee744dc473fc25c16ce
Wanjira <jeanwanjira2802@gmail.com>
2022-12-20 12:23:11 -0800

    generates random valid passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Generates random valid passwords for the
+ *       program 101-crackme.
+ * Return: Always 0.
+*/
+int main(void)
+{
+char passwd[84];
+int i = 0, sum = 0, diff_half1, diff_half2;
+srand(time(0));
+while (sum < 2772)
+{
+passwd[i] = 33 + rand() % 94;
+sum += passwd[i++];
+}
+passwd[i] = '\0';
+if (sum != 2772)
+{
+diff_half1 = (sum - 2772) / 2;
+diff_half2 = (sum - 2772) / 2;
+if ((sum - 2772) % 2 != 0)
+diff_half1++;
+for (i = 0; passwd[i]; i++)
+{
+if (passwd[i] >= (33 + diff_half1))
+{
+passwd[i] -= diff_half1;
+break;
+}
+}	
+for (i = 0; passwd[i]; i++)
+{
+if (passwd[i]>= (33 + diff_half2))
+{
+passwd[i] -= diff_half2;
+break;
+}
+}
+printf("%s", passwd);
+return (0);
+}
