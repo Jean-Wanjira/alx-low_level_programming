3721f939ce05518cb0bc17000e9b1b534691f0ce
Wanjira <jeanwanjira2802@gmail.com>
2022-12-20 13:10:16 -0800

    copies the string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,6 +5,7 @@
  *           terminating null byte, to a buffer pointed to by @dest.
  * @dest: A buffer to copy the string to.
  * @src: The source string to copy.
+ * Return: @dest
 */
 char *_strcpy(char *dest, char *src)
 {
