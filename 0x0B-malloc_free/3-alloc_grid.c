eef5c41395dd9801849e00dd4cadbfdc201cb334
Wanjira <jeanwanjira2802@gmail.com>
2023-01-11 06:46:26 -0800

    a function that returns a pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,11 +27,11 @@ free(twoD[hgt_i]);
 free(twoD);
 return (NULL);
 }
+}
 for (hgt_i = 0; hgt_i < height; hgt_i++)
 {
 for (wid_i = 0; wid_i < width; wid_i++)
 twoD[hgt_i][wid_i] = 0;
-}
 }
 return (twoD);
 }
