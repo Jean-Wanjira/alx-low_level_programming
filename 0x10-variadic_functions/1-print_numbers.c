23832c105b577c816d100d8cabc0143668b87df8
Jean Wanjira <jeanwanjira2802@gmail.com>
2023-01-25 01:34:45 -0800

    Print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ va_start(valist, n);
 for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(valist, int));
-f (separator && i < n - 1)
+if(separator && i < n - 1)
 printf("%s", separator);
 }
 printf("\n");
