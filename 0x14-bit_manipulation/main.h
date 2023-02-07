c287de82b54237b5c4111da9dbaff8f6a0df45df
Jean Wanjira <jeanwanjira2802@gmail.com>
2023-02-07 02:45:34 -0800

    Made some changes in the header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- * main.h -header file that contains the prototypes
-           of functions used in 0x14-bit_manipulation directory
- * Return :nothing
+ * Desc -header file that contains the prototypes
+ *       for all functions used in 0x14-bit_manipulation directory
 */
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -14,4 +13,4 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 int _atoi(const char *s);
 int _putchar(char c);
-#endif 
+#endif
